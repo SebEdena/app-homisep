58c84d6a9a4d15b868bee1e144691e7168d34e31
Laurent Yu <laurentyu1997@hotmail.fr>
2018-11-19 12:04:23 +0100

    changement dans le modèle pour la connexion

--- MVC/modele/connexion.php
+++ MVC/modele/connexion.php
@@ -1,10 +1,27 @@
 <?php
+
+
   function connexionUtilisateur($username,$password,$selecteur)
   {
-    echo($username . "  ");
-    echo($password . "  ");
-    echo(password_hash($password, PASSWORD_DEFAULT). "  ");
-    echo($selecteur);
+    $database = new PDO('mysql:host=localhost;dbname=homisep', "root", "root");
+    if($selecteur == "admin")
+    {
+      $res = $database -> prepare('select * from administrateur where administrateur.mail = ? and administrateur.passe = ?');
+    }
+    else
+    {
+      $res = $database -> prepare('select * from client client.mail = ? and client.passe = ?');
+    }
+
+    echo(password_hash($password, PASSWORD_DEFAULT));
+
+    $hashcode = password_hash($password, PASSWORD_DEFAULT);
+    $res -> bindParam(1, $username);
+    $res -> bindParam(2, $hashcode);
+
+    $res -> execute();
+
+    $queryResult = $res ->fetchAll();
   }
 
 ?>

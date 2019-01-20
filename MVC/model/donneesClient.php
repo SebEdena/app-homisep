6fb2525fb53e6b3ff82e1a32c92c46cd722250ee
StepManlau <laurentyu1997@hotmail.fr>
2019-01-20 10:24:29 +0100

    correctif

--- MVC/model/donneesClient.php
+++ MVC/model/donneesClient.php
@@ -15,40 +15,38 @@
 
 
 
-function recupererDonneesClient() 
+function recupererDonneesClient()
 {
 	require('./model/config.php');
   require('./model/classes/client.php');
 
-try {
-  $id = $_SESSION['id'];
-  $query = $database -> prepare('SELECT * FROM client WHERE idClient = ?');
-  $query -> bindParam(1, $id);
-  $query -> execute();
+  try {
+    $id = $_SESSION['id'];
+    $query = $database -> prepare('SELECT * FROM client WHERE idClient = ?');
+    $query -> bindParam(1, $id);
+    $query -> execute();
 
-  $res = $query->fetchAll(PDO::FETCH_CLASS, 'Client');
-  return $res[0];
-}
-catch(PDOException $Exception) {
-  return false;
-}
+    $res = $query->fetchAll(PDO::FETCH_CLASS, 'Client');
+    return $res[0];
+  }
+  catch(PDOException $Exception) {
+    return false;
+  }
 }
 
 
-function actuDonneesClient($nom, $prenom, $bday, $email, $adresse, $ville, $codePostal, $id) 
+function actuDonneesClient($nom, $prenom, $bday, $email, $adresse, $ville, $codePostal, $id)
 {
   require('./model/config.php');
   require_once('./model/util.php');
-  
-
-    try {
+  try {
     $query = $database -> prepare('UPDATE client SET nom = ?, prenom = ?, dateNaissance = ?, adresse = ?, ville = ?, codePostal = ? WHERE idClient = ?');
     $query -> bindParam(1, $nom);
     $query -> bindParam(2, $prenom);
     $query -> bindParam(3, $bday);
-    $query -> bindParam(4, $adresse); 
-    $query -> bindParam(5, $ville); 
-    $query -> bindParam(6, $codePostal);        
+    $query -> bindParam(4, $adresse);
+    $query -> bindParam(5, $ville);
+    $query -> bindParam(6, $codePostal);
     $query -> bindParam(7, $id);
 
 

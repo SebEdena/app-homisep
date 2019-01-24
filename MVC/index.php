2808d2d82e606882e3ce782d5992588b653f255d
SebEdena <viguier.sebastien@orange.fr>
2019-01-24 22:35:12 +0100

    ajout anti back on deco

--- MVC/index.php
+++ MVC/index.php
@@ -12,6 +12,10 @@ session_start();
 $control = isset($_GET['control'])?$_GET['control']:'connexionInitialisation';
 $action = isset($_GET['action'])?$_GET['action']:'page_connexion';
 
+if(!isset($_SESSION['id']) && $control !== 'connexionInitialisation'){
+    header('Location: index.php');
+}
+
 if(file_exists('./controller/' .  $control . '.php'))
 {
     require ('./controller/' .  $control . '.php');

6668a48b6b4b9d76d0aeabc0c31ae6a0d06fc285
StepManlau <laurentyu1997@hotmail.fr>
2019-06-03 21:44:40 +0200

    ajout des lignes de commandes drop

--- homisepIntegration.sql
+++ homisepIntegration.sql
@@ -16,6 +16,10 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
+drop database if exists `homisep`;
+create database `homisep` CHARSET=utf8;
+use `homisep`;
+
 --
 -- Base de données :  `homisep`
 --

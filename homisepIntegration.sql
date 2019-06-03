fad41d2ce912765d6ce13f61c79cbd71651878cf
StepManlau <laurentyu1997@hotmail.fr>
2019-06-03 21:42:45 +0200

    ajout dans le script intégration

--- homisepIntegration.sql
+++ homisepIntegration.sql
@@ -122,7 +122,7 @@ INSERT INTO `grandeurphysique` (`idGrandeurPhysique`, `nom`, `symbole`, `pas`, `
 
 CREATE TABLE `historique` (
   `idHistorique` int(11) NOT NULL,
-  `dateReleve` datetime NOT NULL,
+  `date` datetime NOT NULL,
   `valeurReleve` float NOT NULL,
   `idCemac` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -131,7 +131,7 @@ CREATE TABLE `historique` (
 -- Contenu de la table `historique`
 --
 
-INSERT INTO `historique` (`idHistorique`, `dateReleve`, `valeurReleve`, `idCemac`) VALUES
+INSERT INTO `historique` (`idHistorique`, `date`, `valeurReleve`, `idCemac`) VALUES
 (1, '2019-06-03 17:12:10', 25, 14),
 (2, '2019-06-03 17:12:12', 36, 15),
 (3, '2019-06-03 17:14:21', 37, 15),
